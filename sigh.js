03ce533cacd6ab27f8c9d2f247d41d8633347570
James Pike <jpike@chilon.net>
2015-05-03 16:24:35 +0000

    uglify each file due to inability to apply on concatted source map.

--- sigh.js
+++ sigh.js
@@ -3,10 +3,14 @@ var merge, babel, concat, debounce, env, glob, write, uglify
 module.exports = function(pipelines) {
   pipelines['js:all'] = [
     merge(
-      [ glob({ basePath: 'src' }, '*.js'), babel({ modules: 'common' }) ],
+      [
+        glob({ basePath: 'src' }, '*.js'),
+        babel({ modules: 'common' }),
+      ],
       glob('bootstrap.js')
     ),
-    env([ debounce(500), concat('combined.js'), uglify() ], 'production'),
+    // uglify should come after concat...
+    env([ uglify(), debounce(500), concat('combined.js') ], 'production'),
     write('dist')
   ]
 }
