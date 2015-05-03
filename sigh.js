3ca79541fd2cef49f3b2cbdc8d2c87598e098f97
James Pike <jpike@chilon.net>
2015-05-03 21:58:03 +0100

    Document reason for uglify position better.

--- sigh.js
+++ sigh.js
@@ -9,7 +9,7 @@ module.exports = function(pipelines) {
       ],
       glob('bootstrap.js')
     ),
-    // uglify should come after concat...
+    // uglify should come after concat... see https://github.com/joyent/node/issues/9445
     env([ uglify(), debounce(500), concat('combined.js') ], 'production'),
     write('dist')
   ]
